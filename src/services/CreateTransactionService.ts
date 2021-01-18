2987b0ad2e5b700e9505690649c6d60a3eba1efa
geraldoaax <geraldoaax@gmail.com>
2021-01-18 14:11:00 -0300

    first

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ income, outcome, total }): Transaction {
+    const transactionDate = startOfHour(date); //regra de negocio.. grava de hora em hora
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate,);
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is alredy booked!')
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    })
+
+    return appointment;
+
   }
+}
 }
 
 export default CreateTransactionService;
